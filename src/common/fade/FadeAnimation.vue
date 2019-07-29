161850cd25de9682beb6f057e764ec043962dc30
YoungT <865428527@qq.com>
2019-07-29 10:24:26 +0800

    finish project

--- src/common/fade/FadeAnimation.vue
+++ src/common/fade/FadeAnimation.vue
@@ -11,7 +11,7 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-    .v-enter, .v-leave-to
+    .v-enter, .v-leave-to 
         opacity 0
     .v-enter-active, .v-leave-active
         transition  opacity .5s
