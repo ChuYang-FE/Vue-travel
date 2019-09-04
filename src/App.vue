861cc60fbc8703727610dd7a75eaad60520e4c75
Young <865428527@qq.com>
2019-09-04 22:41:45 +0800

    修复城市搜索后再次进入城市页仍显示上次搜索的拼音的bug

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <!-- 路由页面做缓存，除了Detail页面（即mouted钩子每次都会被执行）-->
-    <keep-alive exclude="Detail">  
+    <keep-alive exclude="Detail,City">  
       <router-view/> <!-- 路由页面 -->
     </keep-alive>
   </div>
